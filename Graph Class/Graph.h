9dbe115cc09a4c7097472fe38946fc079c57aed0
Larry Liang <lian999111@gmail.com>
2017-08-13 02:45:41 +0800

    Add private attributes

--- Graph Class/Graph.h
+++ Graph Class/Graph.h
@@ -1,9 +1,20 @@
 // Graph class definition
 #pragma once
+
+#include <vector>
+
 class Graph
 {
 public:
 	Graph();
+
+
 	~Graph();
+
+private:
+	int num_of_edges;
+	int num_of_vertices;
+	// graph is a 2D vector storing the edges with their values
+	std::vector<std::vector<int>> graph;
 };
 
