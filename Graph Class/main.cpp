90f73e24cd3f0dfbd8df1cd75f9420d0c268a6f3
Larry Liang <lian999111@gmail.com>
2017-08-13 21:55:34 +0800

    Add test of NeighborOf()

--- Graph Class/main.cpp
+++ Graph Class/main.cpp
@@ -2,9 +2,15 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <vector>
 #include "Graph.h"
+
+using namespace std;
 
 int main()
 {
-	Graph my_graph(5, 0.5, 10);
+	Graph my_graph(5, 0.7, 10);
+	vector<int> neighbor = my_graph.NeighborsOf(3);
+	for (vector<int>::const_iterator i = neighbor.begin(); i != neighbor.end(); ++i)
+		cout << *i << ' ';
 }
