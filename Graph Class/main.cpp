c54b71f0e99ff036552755be6b630209e8646e45
Larry Liang <lian999111@gmail.com>
2017-08-13 22:55:05 +0800

    Test all public methods with captions

--- Graph Class/main.cpp
+++ Graph Class/main.cpp
@@ -13,16 +13,34 @@ int main()
 
 	// Get the neighbors and print
 	vector<int> neighbor = my_graph.NeighborsOf(1);
+	cout << "Neighbors of 1: ";
 	for (vector<int>::const_iterator i = neighbor.begin(); i != neighbor.end(); ++i)
 		cout << *i << ' ';
 	cout << endl;
 
 	// Get the vertices and print
 	vector<int> vertices = my_graph.GetVertices();
+	cout << "All vertices: ";
 	for (vector<int>::const_iterator i = vertices.begin(); i != vertices.end(); ++i)
 		cout << *i << ' ';
 	cout << endl;
 
 	// Check the conntecion
-	cout << my_graph.CheckConnection(1, 2) << endl;
+	cout << "Check the connection between 1 and 2: "
+		<< my_graph.CheckConnection(1, 2) 
+		<< endl;
+
+	// Print the range between 1 and 2, then set it to 5
+	if (my_graph.CheckConnection(1, 2))
+	{
+		cout << "1 and 2 are connected by range: " 
+			<< my_graph.GetEdgeValue(1, 2)
+			<< endl;
+		my_graph.SetEdgeValue(1, 2, 5);
+	}
+
+	// Print the new range
+	cout << "The range is changed to: "
+		<< my_graph.GetEdgeValue(1, 2)
+		<< endl;
 }
