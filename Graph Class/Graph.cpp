7c28617bd282a21c066b9d8154abcab1cf138c20
Larry Liang <lian999111@gmail.com>
2017-08-13 13:05:54 +0800

    Finish Graph constructor

--- Graph Class/Graph.cpp
+++ Graph Class/Graph.cpp
@@ -30,10 +30,16 @@ Graph::Graph(int num_of_vertices, double density, int max_range) :
 	srand(time(0));
 	for (int vert_idx_1 = 0; vert_idx_1 < num_of_vertices; ++vert_idx_1)
 	{
+		// Start looping from the next vertex
 		for (int vert_idx_2 = vert_idx_1 + 1; vert_idx_2 < num_of_vertices; ++vert_idx_2)
 		{
-			edge_matrix_.at(vert_idx_1).at(vert_idx_2) = (rand() % k_max_range_) + 1;
-
+			// If the random number is below density, make no edge and continue to the next loop
+			if (rand() / ((double)RAND_MAX + 1) < density)
+				continue;
+			// Generate the random range
+			int range = (rand() % k_max_range_) + 1;
+			// Add edge
+			assert(AddEdge(vert_idx_1, vert_idx_2, range));
 		}
 	}
 
