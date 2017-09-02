8de0156a458bdce20aa1a6b27687e9136511f64b
Larry Liang <lian999111@gmail.com>
2017-09-02 20:58:49 +0800

    Fix the constructor by using node names instead of index to add edges

--- Graph Class/Graph.h
+++ Graph Class/Graph.h
@@ -149,20 +149,32 @@ Graph<T>::Graph(int num_of_vertices, double density, int max_range)
 
 	// Else, make edges
 	srand(time(0));
-	for (int vert_idx_1 = 0; vert_idx_1 < num_of_vertices; ++vert_idx_1)
+	int i_idx = 0;
+	for (auto&& i_node_name : symbol_table_)
 	{
+		// Find the indices of the i node
+		try
+		{
+			i_idx = GetIndex(i_node_name);
+		}
+		catch (const out_of_range &e)
+		{
+			throw;
+		}
 		// Start looping from the next vertex
-		for (int vert_idx_2 = vert_idx_1 + 1; vert_idx_2 < num_of_vertices; ++vert_idx_2)
+		for (auto ite = symbol_table_.begin() + i_idx + 1; ite != symbol_table_.end(); ite++)
 		{
 			// If the random number is bigger than density, 
 			// make no edge and continue to the next loop
 			double rand_num = rand() / ((double)RAND_MAX + 1);
 			if (rand_num > density)
 				continue;
+
+			auto j_node_name = *ite;
 			// Generate the random range
 			int range = (rand() % k_max_range_) + 1;
 			// Add edge
-			AddEdge(vert_idx_1, vert_idx_2, range);
+			AddEdge(i_node_name, j_node_name, range);
 		}
 	}
 }
@@ -183,27 +195,39 @@ Graph<T>::Graph(const std::vector<T>& vec_node_name, double density = 0.0, int m
 
 	// If density is 0 or no. of vertices is 0,
 	// make no edges and return directly
-	if (num_of_vertices == 0 || density == 0.0)
+	if (num_of_vertices_ == 0 || density == 0.0)
 	{
 		return;
 	}
 
 	// Else, make edges
 	srand(time(0));
-	for (int vert_idx_1 = 0; vert_idx_1 < num_of_vertices; ++vert_idx_1)
+	int i_idx = 0;
+	for (auto&& i_node_name : symbol_table_)
 	{
+		// Find the indices of the i node
+		try
+		{
+			i_idx = GetIndex(i_node_name);
+		}
+		catch (const out_of_range &e)
+		{
+			throw;
+		}
 		// Start looping from the next vertex
-		for (int vert_idx_2 = vert_idx_1 + 1; vert_idx_2 < num_of_vertices; ++vert_idx_2)
+		for (auto ite = symbol_table_.begin() + i_idx + 1; ite != symbol_table_.end(); ite++)
 		{
 			// If the random number is bigger than density, 
 			// make no edge and continue to the next loop
 			double rand_num = rand() / ((double)RAND_MAX + 1);
 			if (rand_num > density)
 				continue;
+
+			auto j_node_name = *ite;
 			// Generate the random range
 			int range = (rand() % k_max_range_) + 1;
 			// Add edge
-			AddEdge(vert_idx_1, vert_idx_2, range);
+			AddEdge(i_node_name, j_node_name, range);
 		}
 	}
 }
