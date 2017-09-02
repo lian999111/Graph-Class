90df53f7a26ea68e8f3d4bfcd10c178c47fcbd3f
Larry Liang <lian999111@gmail.com>
2017-09-02 21:15:19 +0800

    Finish the test of Graph class

--- Graph Class/main.cpp
+++ Graph Class/main.cpp
@@ -3,12 +3,14 @@
 #include <cstdlib>
 #include <iostream>
 #include <vector>
+#include <string>
 #include "Graph.h"
 
 using namespace std;
 
 int main()
 {
+	cout << "Use number of vertices to construct:" << endl;
 	Graph<int> my_graph(5, 0.7, 10);
 
 	cout << "No. of vertices: " << my_graph.GetNumOfVertices() << endl;
@@ -42,6 +44,7 @@ int main()
 			<< my_graph.GetEdgeValue(1, 2)
 			<< endl;
 		my_graph.SetEdgeValue(1, 2, 5);
+		cout << "my_graph.SetEdgeValue(1, 2, 5);" << endl;
 	}
 
 	// Print the new range
@@ -56,4 +59,54 @@ int main()
 		<< my_graph.GetEdgeValue(1, 2)
 		<< endl;
 	
+	/////////// Test a graph with string node names ///////////
+	cout << endl << "Use string node names to construct:" << endl;
+	vector<string> node_names{ "A", "B", "C", "D", "E" };
+	Graph<string> my_string_graph(node_names, 0.7, 10);
+
+	cout << "No. of vertices: " << my_string_graph.GetNumOfVertices() << endl;
+	cout << "No. of edges: " << my_string_graph.GetNumOfEdges() << endl;
+
+	// Get all the vertices and print
+
+	// the difference between declaring using vector<int>& or vector<int>????
+	const auto string_vertices = my_string_graph.GetVertices();
+	cout << "All vertices: ";
+	for (auto i = string_vertices.begin(); i != string_vertices.end(); ++i)
+		cout << *i << ' ';
+	cout << endl;
+
+	// Get the neighbors and print
+	auto string_neighbor = my_string_graph.NeighborsOf("A");
+	cout << "Neighbors of A: ";
+	for (auto i = string_neighbor.begin(); i != string_neighbor.end(); ++i)
+		cout << *i << ' ';
+	cout << endl;
+
+	// Check the conntecion
+	cout << "Check the connection between A and B: "
+		<< my_string_graph.CheckConnection("A", "B")
+		<< endl;
+
+	// Print the range between 1 and 2, then set it to 5
+	if (my_string_graph.CheckConnection("A", "B"))
+	{
+		cout << "1 and 2 are connected by range: "
+			<< my_string_graph.GetEdgeValue("A", "B")
+			<< endl;
+		my_string_graph.SetEdgeValue("A", "B", 5);
+		cout << "my_string_graph.SetEdgeValue(\"A\", \"B\", 5) performed" << endl;
+	}
+
+	// Print the new range
+	cout << "The range is changed to: "
+		<< my_string_graph.GetEdgeValue("A", "B")
+		<< endl;
+
+	// Delete an edge
+	my_string_graph.DeleteEdge("A", "B");
+
+	cout << "1 and 2 are connected by range: "
+		<< my_string_graph.GetEdgeValue("A", "B")
+		<< endl;
 }
